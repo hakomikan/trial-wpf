31a0532a78f73afaebf9787fe69766d86b5b2a49
hakomikan <mail@s-taki.sakura.ne.jp>
2020-05-23 21:43:54 +0900

    add something

--- FirstWpf/FirstWpf/MainWindow.xaml.cs
+++ FirstWpf/FirstWpf/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.IO;
 using System.Windows;
@@ -14,6 +15,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Markup;
+using FirstFloor.ModernUI.Windows.Controls;
+using FirstFloor.ModernUI.Presentation;
 
 namespace FirstWpf
 {
@@ -28,55 +31,101 @@ namespace FirstWpf
         {
             return new MemoryStream(Encoding.UTF8.GetBytes(value ?? ""));
         }
+
+        public static void Retry(Action action, int retryCount, TimeSpan interval)
+        {
+            var exceptions = new List<Exception>();
+
+            for (int i = 0; i < retryCount; i++)
+            {
+                try
+                {
+                    action();
+                    return;
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"Error: {e}");
+                    exceptions.Add(e);
+                    Thread.Sleep(interval);
+                }
+            }
+
+            throw new AggregateException(exceptions);
+        }
+    }
+
+    class TestCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Console.WriteLine($"test: {parameter}");
+        }
     }
 
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : ModernWindow
     {
+        private FileInfo DynamicContentXmlPath = new FileInfo("C:/home/prj/trial-wpf/FirstWpf/FirstWpf/DynamicContent.xaml");
+        private FileSystemWatcher UpdateWatcher;
+
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = new Data();
+            //this.DataContext = new Data();
+
+            //AppearanceManager.Current.ThemeSource = AppearanceManager.DarkThemeSource;
+            AppearanceManager.Current.AccentColor = Colors.Magenta;
+
+            var targetContainer = (this.Content as Grid).FindName("DynamicContent") as Grid;
+            var firstButton = targetContainer.FindName("FirstButton") as Button;
+            firstButton.Command = new TestCommand();
+
+            UpdateWatcher = new FileSystemWatcher()
+            {
+                Filter = "*",
+                IncludeSubdirectories = true,
+                NotifyFilter = NotifyFilters.LastWrite,
+                Path = DynamicContentXmlPath.Directory.FullName
+            };
+            UpdateWatcher.Changed += (f, e) =>
+            {
+                var path = e.FullPath;
+                if (path == DynamicContentXmlPath.FullName)
+                {
+                    Console.WriteLine($"Update: {e.FullPath}");
+                    Utility.Retry(() =>
+                    {
+                        using (var stream = DynamicContentXmlPath.OpenRead())
+                        {
+                            Console.WriteLine($"Update: {e.FullPath}");
+
+                            var dispatcher = Application.Current.Dispatcher;
+
+                            dispatcher.Invoke(() =>
+                            {
+                                var dynamicContent = XamlReader.Load(DynamicContentXmlPath.OpenRead()) as UIElement;
+                                targetContainer.Children.Clear();
+                                targetContainer.Children.Add(dynamicContent);
+                            });
+                        }
+                    }, 20, TimeSpan.FromMilliseconds(100));
+                }
+            };
+            UpdateWatcher.EnableRaisingEvents = true;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine("aaaaaaaaaaaaaaa");
-
-        //    <Window
-        //xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
-        //xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
-        //xmlns:d=""http://schemas.microsoft.com/expression/blend/2008""
-        //xmlns:mc=""http://schemas.openxmlformats.org/markup-compatibility/2006""
-        //xmlns:local=""clr-namespace:FirstWpf""
-        //mc:Ignorable=""d""
-        //Title=""MainWindow"" Height=""356.353"" Width=""525"">
-
-            var xaml = @"
-
-    <Grid 
-        xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
-        xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
-        xmlns:d=""http://schemas.microsoft.com/expression/blend/2008""
-        xmlns:mc=""http://schemas.openxmlformats.org/markup-compatibility/2006""
-
-        Margin=""7,0,0,0"" RenderTransformOrigin=""0.397,0.502"">
-        <Grid.RowDefinitions>
-            <RowDefinition Height=""40""/>
-            <RowDefinition/>
-            <RowDefinition/>
-        </Grid.RowDefinitions>
-        <Button Content=""Button"" HorizontalAlignment=""Left"" Margin=""10,10,0,0"" VerticalAlignment=""Top"" Width=""75""/>
-        <TextBox Text=""asdfghjk"" Grid.Row=""1"" HorizontalAlignment=""Left"" Height=""23"" Margin=""9.5,10,0,0"" TextWrapping=""Wrap"" VerticalAlignment=""Top"" Width=""120"" IsReadOnlyCaretVisible=""True""/>
-        <TextBox Grid.Row=""2"" HorizontalAlignment=""Left"" Height=""22"" Margin=""10,10.5,0,0"" TextWrapping=""Wrap"" Text=""hhhh"" VerticalAlignment=""Top"" Width=""120"" RenderTransformOrigin=""1.934,10.165""/>
-    </Grid>
-";
-
-            var a = XamlReader.Load(Utility.GenerateStreamFromString(xaml));
-
-            this.Content = a;
         }
     }
 }
